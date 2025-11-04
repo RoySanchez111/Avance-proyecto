dc365097491f239b24296c47468b82c6e7c44d12
RoySanchez111 <royismael2022@outlook.com>
2025-11-03 20:29:07 -0600

    tree

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Importar
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Envolver App con BrowserRouter */}
-    <BrowserRouter>
+    {/* BrowserRouter con basename para GitHub Pages */}
+    <BrowserRouter basename="/Avance-proyecto">
       <App />
     </BrowserRouter>
   </React.StrictMode>
